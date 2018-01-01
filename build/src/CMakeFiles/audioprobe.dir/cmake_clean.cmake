587f91a8721964ac3dd3396b7411b17b9e73a5b8
m.sikora@uky.edu <m.sikora@uky.edu>
2018-01-01 16:19:10 +0000

    Initial commit

---
+++ build/src/CMakeFiles/audioprobe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/audioprobe.dir/audioprobe.cpp.o"
+  "../../bin/audioprobe.pdb"
+  "../../bin/audioprobe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/audioprobe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
